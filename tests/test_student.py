83daca32b8c72836819c4bed84f5630065e797a9
Abby Castillo <abby.machines@gmail.com>
2023-04-04 11:24:59 -0700

    implement test test_add_class_adds_class_to_list

--- tests/test_student.py
+++ tests/test_student.py
@@ -29,3 +29,22 @@ def test_get_num_classes_empty_list_returns_0():
 
     assert result == 0
 
+def test_add_class_adds_class_to_list():
+    # arrange
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                [
+                    "Pre-Calc", 
+                    "English III", 
+                    "World History", 
+                    "Gym", 
+                    "Chemistry", 
+                    "Music Composition"
+                ]
+            )    
+    # act
+    quinn.add_class("Underwater basket weaving")
+    # assert
+    assert len(quinn.classes) == 7
+    assert "Underwater basket weaving" in quinn.classes
