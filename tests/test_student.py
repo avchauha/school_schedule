38958599afb1ab86d8cd666c4b97168db90e8dbb
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-04-04 11:14:50 -0700

    implemented testing for empty classes list

--- tests/test_student.py
+++ tests/test_student.py
@@ -17,3 +17,15 @@ def test_instantiate_valid_student():
 
     assert quinn.name == "Quinn"
     assert len(quinn.classes) == 6
+
+def test_get_num_classes_empty_list_returns_0():
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                []
+            )
+    
+    result = quinn.get_num_classes()
+
+    assert result == 0
+
