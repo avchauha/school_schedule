52bd77fa7093e24ed3b4fd7c95ac72bc52397b5a
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-04-04 11:01:18 -0700

    Added info to files

--- school_schedule/student.py
+++ school_schedule/student.py
@@ -1,3 +1,15 @@
 class Student:
-    pass
+    def __init__ (self, name, grade, classes):
+        self.name = name
+        self.grade = grade
+        self.classes = classes
+
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+    
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        print(f"{self.name} is a {self.grade}. {self.name} takes {','.join(self.classes)}")
     
